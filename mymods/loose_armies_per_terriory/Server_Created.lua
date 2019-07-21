672a415a90fefe726a5705b9e18ff5aa97756074
DanWaLes <daniel.church@btinternet.com>
2019-07-21 06:41:03 +0100

    Commerce can't be force enabled :(
    modified mod description accordingly

--- mymods/loose_armies_per_terriory/Server_Created.lua
+++ mymods/loose_armies_per_terriory/Server_Created.lua
@@ -1,13 +1,6 @@
--- force enable commerce and apply bonus overrider if needed
+-- apply bonus overrider if needed
 
 function Server_Created(game, settings)
-	-- force enable commerce
-	if not settings.CommerceGame then
-		settings.CommerceGame = true;
-		settings.CommerceArmyCostMultiplier = 1;
-		settings.CommerceCityBaseCost = nil;
-	end
-
 	-- apply bonus overrider
     local overriddenBonuses = {};
 
