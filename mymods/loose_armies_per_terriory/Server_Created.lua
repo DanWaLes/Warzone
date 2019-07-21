542dc586c4bfe9e928aea72b063efde647d6b5ec
DanWaLes <daniel.church@btinternet.com>
2019-07-21 06:27:51 +0100

    Added mod desciption, force-enable commerce

--- mymods/loose_armies_per_terriory/Server_Created.lua
+++ mymods/loose_armies_per_terriory/Server_Created.lua
@@ -1,6 +1,14 @@
--- modify bonuses to be the old value + number of territories in the bonus
+-- force enable commerce and apply bonus overrider if needed
 
 function Server_Created(game, settings)
+	-- force enable commerce
+	if not settings.CommerceGame then
+		settings.CommerceGame = true;
+		settings.CommerceArmyCostMultiplier = 1;
+		settings.CommerceCityBaseCost = nil;
+	end
+
+	-- apply bonus overrider
     local overriddenBonuses = {};
 
     for i, bonus in pairs(game.Map.Bonuses) do
