7b837f802e329db0c79988d7cbf233d67e784646
DanWaLes <daniel.church@btinternet.com>
2019-07-24 20:29:03 +0100

    Deleted
    Bug fix for making manual distribution games work

--- mymods/lose_armies_for_territories/Server_StartDistribution.lua
+++
@@ -1,7 +0,0 @@
-require "LoseArmiesForTerritories"
-
--- set initial Mod data (manual dist only) - called in manual dist only
-
-function Server_StartDistribution(game, standing)
-	SetInitialStorage(game, standing);
-end
