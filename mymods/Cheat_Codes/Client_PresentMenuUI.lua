e7b2c2d72ddf205cb652a89c8538183c87084a00
DanWaLes <daniel.church@btinternet.com>
2022-11-19 12:11:25 +0000

    Add files via upload
    bug fix for the previous bug fix

--- mymods/Cheat_Codes/Client_PresentMenuUI.lua
+++ mymods/Cheat_Codes/Client_PresentMenuUI.lua
@@ -208,7 +208,7 @@ function makeUseCheatCodeBtns(vert)
 			SendGameCustomMessage('Acknowledging cheat code usage ...', {useCode = cheatCode}, function() end);
 		end;
 
-		if indexOf(Mod.PlayerGameData.codesToUse, cheatCode) > 0 then
+		if Mod.PlayerGameData.codesToUse[cheatCode] > 0 then
 			useCheatCodeBtn.SetInteractable(false);
 		end
 
