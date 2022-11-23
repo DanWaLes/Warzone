5ee7ee354257cad7af5dcf320e9740411db88c54
DanWaLes <daniel.church@btinternet.com>
2022-11-23 09:34:45 +0000

    Add files via upload

--- mymods/Armies_Revolt/Client_PresentSettingsUI.lua
+++ mymods/Armies_Revolt/Client_PresentSettingsUI.lua
@@ -1,16 +1,25 @@
 require 'settings'
+require 'ui'
 
 function Client_PresentSettingsUI(rootParent)
 	cps(rootParent, getSettings());
 end
 
 function cps(rootParent, settings)
-	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	local vert = Vert(rootParent);
 
 	for settingName, setting in pairs(settings) do
-		UI.CreateLabel(vert).SetText(setting.label .. ': '.. tostring(Mod.Settings[settingName]));
+		local settingValue = Mod.Settings[settingName];
 
-		if setting.subsettings and Mod.Settings[settingName] then
+		if settingValue == nil then
+			settingValue = false;
+			-- for settings added while mod is public
+			-- new settings must be a setting with subsettings that is by default not enabled
+		end
+
+		UI.CreateLabel(vert).SetText(setting.label .. ': ' .. settingValue);
+
+		if setting.subsettings and settingValue then
 			cps(vert, setting.subsettings);
 		end
 	end
