08c87139b8db078d48655b3cf1aeb85ec7edd517
DanWaLes <daniel.church@btinternet.com>
2022-12-26 21:33:25 +0000

    Delete Client_GameRefresh.lua

--- mymods/Draw_Resolver/Client_GameRefresh.lua
+++
@@ -1,5 +0,0 @@
-function Client_GameRefresh(game)
-	if (not WL.IsVersionOrHigher or not WL.IsVersionOrHigher('5.22')) then
-		UI.Alert('You must update your app to the latest version to use this mod');
-	end
-end
