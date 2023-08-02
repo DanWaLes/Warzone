d86dfbc1d69c6edbe350b69d704e4b732608bd34
DanWaLes <daniel.church@btinternet.com>
2023-08-02 12:10:12 +0100

    Add files via upload

--- mymods/Advanced Card Distribution per player/Server_Created.lua
+++ mymods/Advanced Card Distribution per player/Server_Created.lua
@@ -1,18 +1,8 @@
 require '_util';
-require '_settings';
 
 function Server_Created(game, settings)
 	if not settings.Cards then
 		return;
-	end
-
-	for id, cardGame in pairs(settings.Cards) do
-		local cardName = string.sub(cardGame.proxyType, #'CardGame' + 1);
-
-		if getSetting('Enable' .. cardName) then
-			settings.Cards[id].Weight = 1;
-			settings.Cards[id].MinimumPiecesPerTurn = 0;
-		end
 	end
 
 	Mod.PublicGameData = {lastGivenCardPiecesOn = {}};
