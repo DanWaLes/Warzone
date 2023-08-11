e3cec3695d77919518d7acef02aba5cd18024104
DanWaLes <daniel.church@btinternet.com>
2023-08-11 10:32:45 +0100

    better skipped order message

--- mymods/Stationary commanders/Server_AdvanceTurn.lua
+++ mymods/Stationary commanders/Server_AdvanceTurn.lua
@@ -25,6 +25,18 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 		return;
 	end
 
+	function skip()
+		skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
+
+		-- show better skipped message
+		local visibility = game.Settings.FogLevel == WL.GameFogLevel.NoFog and nil or {};
+		local orderType = (isAirlift and 'airlift' or 'attack/transfer') .. ' involving commanders';
+		local from = ' from ' .. game.Map.Territories[(isAirlift and order.FromTerritoryID or order.From)].Name;
+		local to = ' to ' .. game.Map.Territories[(isAirlift and order.ToTerritoryID or order.To)].Name;
+
+		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Mod skipped order: ' .. orderType .. from .. to, visibility));
+	end
+
 	if game.Settings.NoSplit then
 		-- if it is no split mode and game has commanders and each player has 1 territory then the game cant progress
 		-- so lets make sure it can progress before skipping orders
@@ -47,7 +59,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 		end
 
 		if canSkip then
-			skipThisOrder(WL.ModOrderControl.Skip);
+			skip();
 			-- dont recreate the order because its no split mode
 		end
 	else
@@ -60,7 +72,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 			newOrder = WL.GameOrderPlayCardAirlift.Create(order.CardInstanceID, order.PlayerID, order.FromTerritoryID, order.ToTerritoryID, numArmies);
 		end
 
-		skipThisOrder(WL.ModOrderControl.Skip);
+		skip();
 		addNewOrder(newOrder);
 	end
 end
