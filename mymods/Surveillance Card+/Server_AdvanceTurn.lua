0398d4e65f8395c94d52733d23c2e2e97d598848
DanWaLes <daniel.church@btinternet.com>
2023-08-12 12:41:31 +0100

    ready for multiplayer testing

---
+++ mymods/Surveillance Card+/Server_AdvanceTurn.lua
@@ -0,0 +1,17 @@
+require '_settings';
+require '_util';
+
+function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
+	if order.proxyType ~= 'GameOrderPlayCardSurveillance' then
+		return;
+	end
+
+	local bonus = game.Map.Bonuses[order.TargetBonus];
+
+	if #bonus.Territories > getSetting('MaxTerrs') then
+		skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
+
+		local visibleTo = game.Settings.CardPlayingsFogged and {} or nil;
+		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Skipped Surveillance Card from being played on ' .. bonus.Name, visibleTo));
+	end
+end
---
+++ mymods/Surveillance Card+/__settings.lua
@@ -0,0 +1,12 @@
+require '_settings'
+require '_ui'
+
+function getSettings()
+	return {
+		addSetting('MaxTerrs', 'Cards can not be played on bonuses with more that this much territories', 'int', 10, {
+			minValue = 2,
+			maxValue = 50,
+			absoluteMax = 4000-- current wz max terrs allowed in map
+		})
+	};
+end
