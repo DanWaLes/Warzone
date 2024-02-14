40f47a29671cccdcd41ec3bb6db8af5a017c97e6
Dan <daniel.church@btinternet.com>
2024-02-14 10:59:53 +0000

    bug fix for wz not correctly updating standing

--- mymods/Wastelands+/Server_AdvanceTurn.lua
+++ mymods/Wastelands+/Server_AdvanceTurn.lua
@@ -1,5 +1,19 @@
-require 'runtimeWastelands'
+require 'runtimeWastelands';
+
+local payload = 'Wastelands+_ServerAdvanceTurnEnd';
+local executed = false;
+
+function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
+	if order.proxyType == 'GameOrderCustom' and order.Payload == payload then
+		skipThisOrder(WL.ModOrderControl.SkipAndSurpessSkippedMessage);
+
+		if not executed then
+			makeRuntimeWastelands(game, addNewOrder);
+			executed = true;
+		end
+	end
+end
 
 function Server_AdvanceTurn_End(game, addNewOrder)
-	makeRuntimeWastelands(game, addNewOrder);
+	addNewOrder(WL.GameOrderCustom.Create(WL.PlayerID.Neutral, '', payload));
 end
