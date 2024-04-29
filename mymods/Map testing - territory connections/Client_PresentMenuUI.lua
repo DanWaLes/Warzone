a84f769d03f4092b6e3a5f9c534f7c9de5904ad0
Dan <daniel.church@btinternet.com>
2024-04-29 13:39:57 +0100

    bug fix

--- mymods/Map testing - territory connections/Client_PresentMenuUI.lua
+++ mymods/Map testing - territory connections/Client_PresentMenuUI.lua
@@ -5,6 +5,12 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close
 	setMaxSize(400, 200);
 
 	local vert = Vert(rootParent);
+
+	if not game.Settings.MapTestingGame then
+		Label(vert).SetText('This mod can only be used in map testing games');
+		return;
+	end
+
 	local numTerrs = #Mod.PublicGameData.terrNames;
 
 	Label(vert).SetText('All territory connections will tested by the end of turn ' .. math.ceil(numTerrs / Mod.PublicGameData.numPlayers));
