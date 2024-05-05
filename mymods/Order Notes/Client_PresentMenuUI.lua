00ce48e8fa9f34862f71769e7abafb293768f3a4
Dan <daniel.church@btinternet.com>
2024-05-05 12:51:26 +0100

    trying to find out what causes browser crash

--- mymods/Order Notes/Client_PresentMenuUI.lua
+++ mymods/Order Notes/Client_PresentMenuUI.lua
@@ -1,77 +1,76 @@
-require('util');
-require('_ui');
-require('placeOrderInCorrectPosition');
+-- require 'util';
+-- require '_ui';
+-- require 'placeOrderInCorrectPosition';
 
 local WIDTH = 800;
 local HEIGHT = 190;
 
--- text gets shrunk when inputting, so limit note size so that it doesnt shrink 2024-04-29
+--[[text gets shrunk when inputting, so limit note size so that it doesnt shrink 2024-04-29]]
 local CHARLIMIT = 50;
 
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
-	print('in Client_PresentMenuUI')
 	setMaxSize(WIDTH, HEIGHT);
 	setScrollable(false, true);
 
-	main(rootParent, nil, game);
+	-- main(rootParent, nil, game);
 end
 
-function main(rootParent, vert, game)
-	print('in main')
-	if not UI.IsDestroyed(vert) then
-		UI.Destroy(vert);
-	end
+-- function main(rootParent, vert, game)
+	-- if not UI.IsDestroyed(vert) then
+		-- UI.Destroy(vert);
+	-- end
 
-	vert = Vert(rootParent);
+	-- vert = Vert(rootParent);
 
-	if game.Game.State ~= WL.GameState.Playing then
-		Label(vert).SetText('You can not use this mod because the game is not in-progress');
-		return;
-	if not game.Us or (game.Us and game.Us.State ~= WL.GamePlayerState.Playing) then
-		Label(vert).SetText('You can not use this mod because you are not in this game');
-		return;
-	end
+	-- if game.Game.State ~= WL.GameState.Playing then
+		-- Label(vert).SetText('You can not use this mod because the game is not in-progress');
+		-- return;
+	-- if not game.Us or (game.Us and game.Us.State ~= WL.GamePlayerState.Playing) then
+		-- Label(vert).SetText('You can not use this mod because you are not in this game');
+		-- return;
+	-- end
 
-	local horz = Horz(vert)
-		.SetPreferredWidth(WIDTH)
-		.SetFlexibleWidth(1);
+	-- local horz = Horz(vert)
+		-- .SetPreferredWidth(WIDTH)
+		-- .SetFlexibleWidth(1);
 
-	Label(horz).SetText('Note')
-	Label(horz).SetText(' | ');
+	-- Label(horz).SetText('Note')
+	-- Label(horz).SetText(' | ');
 
-	local orderNotesSubmitBtn = Btn(horz);
-	local orderNotesTextInput = TextInput(vert);
+	-- local orderNotesSubmitBtn = Btn(horz);
+	-- local orderNotesTextInput = TextInput(vert);
 
-	orderNotesSubmitBtn
-		.SetText('Add note')
-		.SetOnClick(function ()
-			orderNotesSubmitBtn.SetInteractable(false);
-			orderNotesTextInput.SetInteractable(false);
+	-- orderNotesSubmitBtn
+		-- .SetText('Add note')
+		-- .SetOnClick(function ()
+			-- orderNotesSubmitBtn.SetInteractable(false);
+			-- orderNotesTextInput.SetInteractable(false);
 
-			local txt = orderNotesTextInput.GetText();
-			orderNotesTextInput.SetText('');
+			-- local txt = orderNotesTextInput.GetText();
+			-- orderNotesTextInput.SetText('');
 
-			if (#txt < #('A')) or (#txt > CHARLIMIT) then
-				orderNotesTextInput.SetInteractable(true);
-				orderNotesSubmitBtn.SetInteractable(true);
-				return;
-			end
+			-- if (#txt < #('A')) or (#txt > CHARLIMIT) then
+				-- orderNotesTextInput.SetInteractable(true);
+				-- orderNotesSubmitBtn.SetInteractable(true);
+				-- return;
+			-- end
 
-			local order = WL.GameOrderCustom.Create(game.Us.PlayerID, txt, 'OrderNotes');
-			placeOrderInCorrectPosition(game, order);
+			-- local order = WL.GameOrderCustom.Create(game.Us.PlayerID, txt, 'OrderNotes');
+			-- placeOrderInCorrectPosition(game, order);
 
-			orderNotesTextInput.SetInteractable(true);
-			orderNotesSubmitBtn.SetInteractable(true);
-		end);
+			-- orderNotesTextInput.SetInteractable(true);
+			-- orderNotesSubmitBtn.SetInteractable(true);
+		-- end);
 
-	orderNotesTextInput
-		.SetPlaceholderText('Enter note here; max ' .. tostring(CHARLIMIT) .. ' characters')
-		.SetCharacterLimit(CHARLIMIT)
-		.SetPreferredWidth(WIDTH)
-		-- .SetPreferredHeight(HEIGHT)
-		.SetFlexibleWidth(1)
-		-- .SetFlexibleHeight(1);
+	-- orderNotesTextInput
+		-- .SetPlaceholderText('Enter note here; max ' .. tostring(CHARLIMIT) .. ' characters')
+		-- .SetCharacterLimit(CHARLIMIT)
+		-- .SetPreferredWidth(WIDTH)
+		--[[.SetPreferredHeight(HEIGHT)]]
+		-- .SetFlexibleWidth(1)
+		--[[.SetFlexibleHeight(1);]]
 
+--[[
 	-- local i = 1;
 	-- local str = '';
 
@@ -85,4 +84,5 @@ function main(rootParent, vert, game)
 
 	-- orderNotesTextInput.SetText(str);
 	-- Label(mainVert).SetText(str);
-end
+-- ]]
+-- end
