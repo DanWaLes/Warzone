2b125b271ca34020893da456992dacfcf43b7c11
Dan <daniel.church@btinternet.com>
2024-06-19 17:29:44 +0100

    Update version.lua bug fix for super old clients

--- mymods/Territories without armies become neutral/version.lua
+++ mymods/Territories without armies become neutral/version.lua
@@ -2,7 +2,7 @@ require '__mod';
 
 function canRunMod()
 	if (not WL.IsVersionOrHigher or not WL.IsVersionOrHigher(MOD.wzVersion)) then
-		UI.Alert('You must be running app version ' + MOD.wzVersion + ' at the minimum to use mod "' + MOD.name + '". Check for updates.');
+		UI.Alert and UI.Alert('You must be running app version ' + MOD.wzVersion + ' at the minimum to use mod "' + MOD.name + '". Check for updates.');
 		return;
 	end
 
