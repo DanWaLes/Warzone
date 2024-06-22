0d3cffdadf73bd13a97d98bf4835129d5485debb
Dan <daniel.church@btinternet.com>
2024-06-22 17:24:05 +0100

    Update version.lua bug fix

--- mymods/Map testing - territory connections/version.lua
+++ mymods/Map testing - territory connections/version.lua
@@ -1,12 +1,23 @@
--- https://www.warzone.com/wiki/Mod_API_Reference:IncomeMod
+-- https://www.warzone.com/wiki/Mod_API_Reference#Newer_API_features
 
 function canRunMod()
 	local version = '5.17';
-	local name = '"Map testing - territory connections"';
+	local name = '"Map testing - territory connection"';
 
 	if (not WL.IsVersionOrHigher or not WL.IsVersionOrHigher(version)) then
-		UI.Alert('You must be running app version ' + version + ' at the minimum to use mod ' + name + '. Check for updates');
-		return;
+		if UI and UI.Alert then
+			UI.Alert('You must be running app version ' .. version .. ' at the minimum to use mod ' .. name .. '. Check for updates');
+		end
+
+		return false;
+	end
+
+	return true;
+end
+
+function serverCanRunMod(game)
+	if game.Settings.SinglePlayer and not canRunMod() then
+		return false;
 	end
 
 	return true;
