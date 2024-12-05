c86b6ac9ee5d5d3f29ca36d83f6b5b0d3c4f12f8
Dan <daniel.church@btinternet.com>
2024-12-05 22:47:58 +0000

    update AutoSettingsFiles
    add addSettingGroup()
    add optional 'absoluteMinValue' field to addSetting() when using type 'int' or 'float'
    greater backwards compatibility
    remove dependence of other libraries

---
+++ mods/libs/AutoSettingsFiles/code/Client_PresentSettingsUI.lua
@@ -0,0 +1,130 @@
+require('__settings');
+
+function Client_PresentSettingsUI(rootParent)
+	cps(rootParent, getSettings(), 0);
+end
+
+function cps(rootParent, settings)
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+
+	for _, setting in ipairs(settings) do
+		if setting.isTemplate then
+			local n = 1;
+
+			while n < ((Mod.Settings[setting.name] or setting.bkwrds) + 1) do
+				cpsDoSetting(vert, setting.get(n));
+				n = n + 1;
+			end
+		else
+			cpsDoSetting(vert, setting);
+		end
+	end
+end
+
+function cpsDoSetting(vert, setting)
+	local vert2 = UI.CreateVerticalLayoutGroup(vert);
+	local horz = UI.CreateHorizontalLayoutGroup(vert2);
+
+	if setting.isGroup then
+		local btn = UI.CreateButton(horz).SetText(setting.btnText);
+
+		if setting.btnColor then
+			btn.SetColor(setting.btnColor);
+		end
+
+		if setting.btnTextColor then
+			btn.SetTextColor(setting.btnTextColor);
+		end
+
+		btn.SetOnClick(function()
+			settingGroupBtnClicked(btn, vert2, setting);
+		end);
+
+		return;
+	end
+
+	local settingValue = Mod.Settings[setting.name];
+	local label = UI.CreateLabel(horz).SetText(setting.label .. ': ');
+
+	if setting.labelColor then
+		label.SetColor(setting.labelColor);
+	end
+
+	createHelpBtn(horz, vert2, setting);
+
+	if settingValue == nil and setting.bkwrds ~= nil then
+		settingValue = setting.bkwrds;
+	end
+
+	UI.CreateLabel(horz).SetText(tostring(settingValue));
+
+	if not (setting.subsettings and settingValue) then
+		return;
+	end
+
+	local btn = UI.CreateButton(horz).SetText(getExpandBtnLabelTxt()).SetColor('#23A0FF');
+
+	btn.SetOnClick(function()
+		expandCollapseSubSettingBtnClicked(btn, vert2, setting);
+	end);
+end
+
+local settingHelpAreas = {};
+
+function createHelpBtn(btnParent, helpParent, setting)
+	if not setting.help then
+		return;
+	end
+
+	UI.CreateButton(btnParent).SetText('?').SetColor('#23A0FF').SetOnClick(function()
+		if not (WL and WL.IsVersionOrHigher and WL.IsVersionOrHigher('5.21')) then
+			if not settingHelpAreas[setting.name] then
+				settingHelpAreas[setting.name] = UI.CreateVerticalLayoutGroup(helpParent);
+				setting.help(settingHelpAreas[setting.name]);
+			end
+		elseif UI.IsDestroyed(settingHelpAreas[setting.name]) then
+			settingHelpAreas[setting.name] = UI.CreateVerticalLayoutGroup(helpParent);
+			setting.help(settingHelpAreas[setting.name]);
+		else
+			UI.Destroy(settingHelpAreas[setting.name]);
+		end
+	end);
+end
+
+local subsettingsAreas = {};
+
+function expandCollapseSubSettingBtnClicked(btn, detailsParent, setting)
+	if not (WL and WL.IsVersionOrHigher and WL.IsVersionOrHigher('5.21')) then
+		if not subsettingsAreas[setting.name] then
+			subsettingsAreas[setting.name] = UI.CreateVerticalLayoutGroup(detailsParent);
+			cps(subsettingsAreas[setting.name], setting.subsettings);
+		end
+	elseif UI.IsDestroyed(subsettingsAreas[setting.name]) then
+		btn.SetText(getCollapseBtnLabelTxt());
+		subsettingsAreas[setting.name] = UI.CreateVerticalLayoutGroup(detailsParent);
+		cps(subsettingsAreas[setting.name], setting.subsettings);
+	else
+		UI.Destroy(subsettingsAreas[setting.name]);
+		subsettingsAreas[setting.name] = nil;
+		btn.SetText(getExpandBtnLabelTxt());
+	end
+end
+
+local settingGroupAreas = {};
+
+function settingGroupBtnClicked(btn, detailsParent, setting)
+	if not (WL and WL.IsVersionOrHigher and WL.IsVersionOrHigher('5.21')) then
+		if not settingGroupAreas[setting.ID] then
+			settingGroupAreas[setting.ID] = UI.CreateVerticalLayoutGroup(detailsParent);
+			setting.onExpand(btn, UI.CreateVerticalLayoutGroup(settingGroupAreas[setting.ID]));
+			cps(settingGroupAreas[setting.ID], setting.subsettings);
+		end
+	elseif UI.IsDestroyed(settingGroupAreas[setting.ID]) then
+		settingGroupAreas[setting.ID] = UI.CreateVerticalLayoutGroup(detailsParent);
+		setting.onExpand(UI.CreateVerticalLayoutGroup(settingGroupAreas[setting.ID]));
+		cps(settingGroupAreas[setting.ID], setting.subsettings);
+	else
+		UI.Destroy(settingGroupAreas[setting.ID]);
+		settingGroupAreas[setting.ID] = nil;
+	end
+end
---
+++ mods/libs/AutoSettingsFiles/code/__settings.lua
@@ -0,0 +1,44 @@
+require('libs/settings');
+
+function getSettings()
+	return {
+--		addSetting(string, 'int', number, {
+--			minValue = number,
+--			maxValue = number,
+--			absoluteMax = number or nil,
+--			absoluteMin = number or nil,
+--			labelColor = string or nil,
+--			help = function or nil
+--		}),
+--		addSetting(string, 'float', number, {
+--			dp = int,
+--			minValue = number,
+--			maxValue = number,
+--			absoluteMax = number or nil,
+--			absoluteMin = number or nil,
+--			labelColor = string or nil,
+--			help = function or nil
+--		}),
+--		addSetting(string, 'bool', boolean, {
+--			subsettings = Array<setting> or nil
+--			labelColor = string or nil,
+--			help = function or nil
+--		} or nil),
+--		addSetting(string, 'text', string, {
+--			placeholder = string or nil,
+--			charLimit = string or nil,
+--			labelColor = string or nil,
+--			help = function or nil
+--		}),
+--		addSettingGroup(string, nil, {
+--			addSetting(string, 'int', number, {
+--				minValue = number,
+--				maxValue = number,
+--				absoluteMax = number or nil,
+--				absoluteMin = number or nil,
+--				labelColor = string or nil,
+--				help = function or nil
+--			})
+--		})
+	};
+end
