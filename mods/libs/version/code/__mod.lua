299299d2916249059bba6071b8c62d8ff0cc5a48
Dan <daniel.church@btinternet.com>
2024-12-08 18:49:51 +0000

    add version lib
    displays consistent error messages if app version is too low when functions called

---
+++ mods/libs/version/code/__mod.lua
@@ -0,0 +1,7 @@
+-- https://www.warzone.com/wiki/Mod_API_Reference#Newer_API_features
+
+MOD = {
+	name = 'enter mod name here',
+	clientVersion = '5.17',
+	serverVersion = '5.17'
+};
---
+++ mods/libs/version/code/version.lua
@@ -0,0 +1,41 @@
+require('__mod');
+
+local function isVersionOrHigher(version)
+	return WL and WL.IsVersionOrHigher and WL.IsVersionOrHigher(version);
+end
+
+local function err(msg)
+	local alert = (UI and UI.Alert) or error;
+
+	if type(alert) == 'function' then
+		alert(msg);
+	end
+end
+
+function canRunMod()
+	-- for use on client hooks
+	-- not needed in Client_PresentSettingsUI in AutoSettingsFiles due to checking for 5.21
+	-- if is under 5.21 then UI.Destroy and UI.IsDestroyed are never called
+
+	if isVersionOrHigher(MOD.clientVersion) then
+		return true;
+	end
+
+	err('You must be running app version ' .. MOD.clientVersion .. ' at the minimum to use the mod "' .. MOD.name .. '". Check for updates.');
+
+	return false;
+end
+
+function serverCanRunMod(game)
+	-- for use on server hooks
+
+	if game.Settings.SinglePlayer and not isVersionOrHigher(MOD.serverVersion) then
+		-- in single player games the server is the client. client version of mod api framework might not be up to date
+		err('You must be running app version ' .. MOD.serverVersion .. ' at the minimum to use the mod "' .. MOD.name .. '" in single player. Check for updates.');
+
+		return false;
+	end
+
+	-- server always runs most up to date version of mod api framework
+	return true;
+end
