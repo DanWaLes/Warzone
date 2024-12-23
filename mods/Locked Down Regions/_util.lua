599a234b968dfa103ef90464bb78387134e87768
Dan <daniel.church@btinternet.com>
2024-12-23 17:37:07 +0000

    remove placeOrderInCorrectPosition from _util.lua as its not used

--- mods/Locked Down Regions/_util.lua
+++ mods/Locked Down Regions/_util.lua
@@ -52,31 +52,3 @@ function toCaseInsensativePattern(str)
 		return string.format("[%s%s]", string.lower(c), string.upper(c));
 	end);
 end
-
-function placeOrderInCorrectPosition(clientGame, newOrder)
-	if not newOrder.OccursInPhase then
-		local orders = clientGame.Orders;
-		table.insert(orders, newOrder);
-		clientGame.Orders = orders;
-	else
-		local orders = {};
-		local addedNewOrder = false;
-
-		for _, order in pairs(clientGame.Orders) do
-			if order.OccursInPhase then
-				if not addedNewOrder and order.OccursInPhase > newOrder.OccursInPhase then
-					table.insert(orders, newOrder);
-					addedNewOrder = true;
-				end
-
-				table.insert(orders, order);
-			end
-		end
-
-		if not addedNewOrder then
-			table.insert(orders, newOrder);
-		end
-
-		clientGame.Orders = orders;
-	end
-end
