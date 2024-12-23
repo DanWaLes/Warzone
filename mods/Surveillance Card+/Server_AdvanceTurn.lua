153ff426cd911e7528f172a28fc2df5209198fa6
Dan <daniel.church@btinternet.com>
2024-12-23 23:06:07 +0000

    Update Server_AdvanceTurn.lua formatting

--- mods/Surveillance Card+/Server_AdvanceTurn.lua
+++ mods/Surveillance Card+/Server_AdvanceTurn.lua
@@ -1,5 +1,5 @@
-require '_settings';
-require '_util';
+require('_settings');
+require('_util');
 
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	if order.proxyType ~= 'GameOrderPlayCardSurveillance' then
@@ -12,6 +12,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 		skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
 
 		local visibleTo = game.Settings.CardPlayingsFogged and {} or nil;
+
 		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Skipped Surveillance Card from being played on ' .. bonus.Name, visibleTo));
 	end
 end
