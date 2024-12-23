8cacea9cda0df17dccba141f769650ce6a8a25db
Dan <daniel.church@btinternet.com>
2024-12-23 22:15:25 +0000

    Update Server_AdvanceTurn.lua

--- mods/Order Notes/Server_AdvanceTurn.lua
+++ mods/Order Notes/Server_AdvanceTurn.lua
@@ -2,6 +2,6 @@ require('tblprint');
 
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	if order.proxyType == 'GameOrderCustom' and order.Payload == 'OrderNotes' then
-		skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage)
+		skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
 	end
 end
