649a529fc3a7923f2c86f5461b63011b71b9ac3c
Dan <daniel.church@btinternet.com>
2024-12-24 22:51:56 +0000

    Delete mymods/wiki_print directory
    is not a mod

--- mymods/wiki_print/Client_PresentMenuUI.lua
+++
@@ -1,5 +0,0 @@
-require '_util';
-
-function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
-	placeOrderInCorrectPosition(game, WL.GameOrderCustom.Create(game.Us.ID, 'message', 'payload', {[WL.ResourceType.Gold] = 1}));
-end
