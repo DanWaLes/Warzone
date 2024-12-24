ee5ea6d2776472fa0c940f8d503c39bbc89f782b
Dan <daniel.church@btinternet.com>
2024-12-24 11:56:13 +0000

    update to use latest lib version

---
+++ mods/Map testing - territory connections/__mod.lua
@@ -0,0 +1,10 @@
+-- modified from https://github.com/DanWaLes/Warzone/tree/master/mods/libs/version
+
+-- https://www.warzone.com/wiki/Mod_API_Reference#Newer_API_features
+-- WL.IncomeMod
+
+MOD = {
+	name = 'Map testing - territory connections',
+	clientVersion = '5.17',
+	serverVersion = '5.17'
+};
--- mods/Map testing - territory connections/version.lua
+++ mods/Map testing - territory connections/version.lua
@@ -1,24 +1,43 @@
--- https://www.warzone.com/wiki/Mod_API_Reference#Newer_API_features
+-- copied from https://github.com/DanWaLes/Warzone/tree/master/mods/libs/version
+
+require('__mod');
+
+local function isVersionOrHigher(version)
+	return WL and WL.IsVersionOrHigher and WL.IsVersionOrHigher(version);
+end
+
+local function err(msg)
+	local alert = (UI and UI.Alert) or error;
+
+	if type(alert) == 'function' then
+		alert(msg);
+	end
+end
 
 function canRunMod()
-	local version = '5.17';
-	local name = '"Map testing - territory connections"';
+	-- for use on client hooks
+	-- not needed in Client_PresentSettingsUI in AutoSettingsFiles due to checking for 5.21
+	-- if is under 5.21 then UI.Destroy and UI.IsDestroyed are never called
 
-	if (not WL.IsVersionOrHigher or not WL.IsVersionOrHigher(version)) then
-		if UI and UI.Alert then
-			UI.Alert('You must be running app version ' .. version .. ' at the minimum to use mod ' .. name .. '. Check for updates');
-		end
-
-		return false;
+	if isVersionOrHigher(MOD.clientVersion) then
+		return true;
 	end
 
-	return true;
+	err('You must be running app version ' .. MOD.clientVersion .. ' at the minimum to use the mod "' .. MOD.name .. '". Check for updates.');
+
+	return false;
 end
 
 function serverCanRunMod(game)
-	if game.Settings.SinglePlayer and not canRunMod() then
+	-- for use on server hooks
+
+	if game.Settings.SinglePlayer and not isVersionOrHigher(MOD.serverVersion) then
+		-- in single player games the server is the client. client version of mod api framework might not be up to date
+		err('You must be running app version ' .. MOD.serverVersion .. ' at the minimum to use the mod "' .. MOD.name .. '" in single player. Check for updates.');
+
 		return false;
 	end
 
+	-- server always runs most up to date version of mod api framework
 	return true;
 end
