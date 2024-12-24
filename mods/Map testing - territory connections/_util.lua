909c130c334793fd08daf31bce51bd8069ec0c4d
Dan <daniel.church@btinternet.com>
2024-12-24 12:00:58 +0000

    remove unused functions

--- mods/Map testing - territory connections/_util.lua
+++ mods/Map testing - territory connections/_util.lua
@@ -56,13 +56,6 @@ function tblprint(tbl)
 	print(tprint(tbl));
 end
 
-function round(n, dp)
-	-- http://lua-users.org/wiki/SimpleRound
-	local multi = 10 ^ (dp or 0);
-
-	return math.floor((n * multi + 0.5)) / multi;
-end
-
 function numKeys(tbl)
 	local n = 0;
 
