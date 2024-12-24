fe25b54be9618015791670c2c50f182108db87ea
Dan <daniel.church@btinternet.com>
2024-12-24 18:25:32 +0000

    Update __mod.lua

--- mods/Territories without armies become neutral/__mod.lua
+++ mods/Territories without armies become neutral/__mod.lua
@@ -1,6 +1,10 @@
+-- modified from https://github.com/DanWaLes/Warzone/tree/master/mods/libs/version
+
+-- https://www.warzone.com/wiki/Mod_API_Reference#Newer_API_features
+-- https://www.warzone.com/wiki/Mod_Hooks Server_AdvanceTurn_Order addNewOrder(newOrder, true) 5.17
+
 MOD = {
 	name = 'Territories without armies become neutral',
-	wzVersion = '5.17'
+	clientVersion = '5.17',
+	serverVersion = '5.17'
 };
-
--- https://www.warzone.com/wiki/Mod_Hooks Server_AdvanceTurn_Order addNewOrder(newOrder, true) 5.17
