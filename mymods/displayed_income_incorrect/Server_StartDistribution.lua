aaac3d906a02d20a031910fd89f83c100c0c728a
Dan <daniel.church@btinternet.com>
2024-12-24 22:51:32 +0000

    Delete mymods/displayed_income_incorrect directory
    is not a mod

--- mymods/displayed_income_incorrect/Server_StartDistribution.lua
+++
@@ -1,9 +0,0 @@
-require "Util"
-
--- set initial Mod data (manual dist) - called in manual dist only
-
-function Server_StartDistribution(game, standing)
-	print("init Server_StartDistribution manualdist");
-	
-	Util_SetInitialStorage(game, "enteredServer_StartDist");
-end
