99cdc94145870086732a15531198cea55510a7df
Dan <daniel.church@btinternet.com>
2025-01-25 12:25:23 +0000

    add mod Mystery Card
    is a test case for `addCustomCard` for lib AutoSettingsFiles

---
+++ mods/Mystery Card/__mod.lua
@@ -0,0 +1,9 @@
+-- modified from https://github.com/DanWaLes/Warzone/tree/master/mods/libs/version
+
+-- https://www.warzone.com/wiki/Mod_API_Reference#Newer_API_features
+
+MOD = {
+	name = 'Mystery Card',
+	clientVersion = '5.32',
+	serverVersion = '5.32'
+};
---
+++ mods/Mystery Card/__settings.lua
@@ -0,0 +1,47 @@
+-- modified from https://github.com/DanWaLes/Warzone/tree/master/mods/libs/AutoSettingsFiles
+
+require('settings');
+require('ui');
+
+function getSettings()
+	return {
+		addCustomCard(
+			'MysteryCardID',
+			'Mystery Card',
+			'Grants a player a full random card (excluding Mystery Cards themselves) when played',
+			'MysteryCard.png',
+			{
+				NumPieces = 'NumPieces',
+				Weight = 'Weight',
+				MinimumPiecesPerTurn = 'MinimumPiecesPerTurn',
+				InitialPieces = 'InitialPieces'
+			},
+			{
+				addSetting('NumPieces', 'Number of pieces to divide the card into', 'int', 7, {
+					minValue = 0,
+					maxValue = 20,
+					absoluteMax = 1000
+				}),
+				addSetting('Weight', 'Weight', 'float', 1, {
+					dp = 10,
+					minValue = 0,
+					maxValue = 5,
+					absoluteMax = 1000,
+					help = function(parent)
+						Label(parent).SetText('How common the card is');
+					end
+				}),
+				addSetting('MinimumPiecesPerTurn', 'Minimum pieces awarded per turn', 'int', 8, {
+					minValue = 0,
+					maxValue = 5,
+					absoluteMax = 1000
+				}),
+				addSetting('InitialPieces', 'Pieces given to each player at the start', 'int', 0, {
+					minValue = 0,
+					maxValue = 5,
+					absoluteMax = 1000
+				})
+			}
+		)
+	};
+end
