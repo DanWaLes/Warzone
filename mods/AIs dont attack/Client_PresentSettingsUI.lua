00ba6134c43ce468d5cd81914d65619b61bc35f2
Dan <daniel.church@btinternet.com>
2025-02-25 14:30:33 +0000

    apply AutoSettingsFiles fixes to AIs dont attack

--- mods/AIs dont attack/Client_PresentSettingsUI.lua
+++ mods/AIs dont attack/Client_PresentSettingsUI.lua
@@ -2,7 +2,11 @@
 
 require('__settings');
 
+local canUseUIElementIsDestroyed;
+
 function Client_PresentSettingsUI(rootParent)
+	canUseUIElementIsDestroyed = WL and WL.IsVersionOrHigher and WL.IsVersionOrHigher('5.21');
+
 	cps(rootParent, getSettings(), 0);
 end
 
@@ -79,11 +83,19 @@ function cpsDoSetting(vert, setting)
 		return;
 	end
 
-	local btn = UI.CreateButton(horz).SetText(getExpandBtnLabelTxt()).SetColor('#23A0FF');
+	if canUseUIElementIsDestroyed then
+		local btn = UI.CreateButton(horz).SetText(getExpandBtnLabelTxt()).SetColor('#23A0FF');
 
-	btn.SetOnClick(function()
-		expandCollapseSubSettingBtnClicked(btn, vert2, setting);
-	end);
+		btn.SetOnClick(function()
+			expandCollapseSubSettingBtnClicked(btn, vert2, setting);
+		end);
+	else
+		-- pressing the expand button more than once after the game has been loaded prevents
+		-- sub settings from being viewed if the sub setting was ever viewed
+		-- so display the sub settings fully
+
+		cps(UI.CreateVerticalLayoutGroup(vert2), setting.subsettings);
+	end
 end
 
 local settingHelpAreas = {};
@@ -94,7 +106,7 @@ function createHelpBtn(btnParent, helpParent, setting)
 	end
 
 	UI.CreateButton(btnParent).SetText('?').SetColor('#23A0FF').SetOnClick(function()
-		if not (WL and WL.IsVersionOrHigher and WL.IsVersionOrHigher('5.21')) then
+		if not canUseUIElementIsDestroyed then
 			if not settingHelpAreas[setting.name] then
 				settingHelpAreas[setting.name] = UI.CreateVerticalLayoutGroup(helpParent);
 				setting.help(settingHelpAreas[setting.name]);
@@ -110,13 +122,8 @@ end
 
 local subsettingsAreas = {};
 
-function expandCollapseSubSettingBtnClicked(btn, detailsParent, setting)
-	if not (WL and WL.IsVersionOrHigher and WL.IsVersionOrHigher('5.21')) then
-		if not subsettingsAreas[setting.name] then
-			subsettingsAreas[setting.name] = UI.CreateVerticalLayoutGroup(detailsParent);
-			cps(subsettingsAreas[setting.name], setting.subsettings);
-		end
-	elseif UI.IsDestroyed(subsettingsAreas[setting.name]) then
+function expandCollapseSubSettingBtnClicked(btn, detailsParent, setting)	
+	if UI.IsDestroyed(subsettingsAreas[setting.name]) then
 		btn.SetText(getCollapseBtnLabelTxt());
 		subsettingsAreas[setting.name] = UI.CreateVerticalLayoutGroup(detailsParent);
 		cps(subsettingsAreas[setting.name], setting.subsettings);
