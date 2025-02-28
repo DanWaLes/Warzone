56caad904ed499c045f73e627171565b5dac9cf9
Dan <daniel.church@btinternet.com>
2025-02-28 15:57:09 +0000

    Update __mod.lua
    add Client_PresentCommercePurchaseUI version requirements

--- mods/Custom Card Package 2/__mod.lua
+++ mods/Custom Card Package 2/__mod.lua
@@ -4,6 +4,7 @@
 -- https://www.warzone.com/wiki/Mod_API_Reference:FogMod 5.33
 -- https://www.warzone.com/wiki/Mod_API_Reference:Custom_Cards 5.32.0.1
 -- https://www.warzone.com/wiki/Mod_Hooks#Client_Hooks Client_GameOrderCreated 5.32
+-- https://www.warzone.com/wiki/Mod_Hooks#Client_Hooks Client_PresentCommercePurchaseUI 5.21
 -- https://www.warzone.com/wiki/Mod_API_Reference:UI UI.InterceptNextTerritoryClick 5.17.0
 -- https://www.warzone.com/wiki/Mod_Hooks Server_AdvanceTurn_Order addNewOrder arg2 5.17.0
 
