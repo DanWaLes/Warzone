47e45ca953eb160d73fb083ab261d1b4a9ee4019
Dan <daniel.church@btinternet.com>
2025-02-28 12:25:11 +0000

    Update __settings.lua
    more clarity

--- mods/Surveillance Card+/__settings.lua
+++ mods/Surveillance Card+/__settings.lua
@@ -4,7 +4,7 @@ require('settings');
 
 function getSettings()
 	return {
-		addSetting('MaxTerrs', 'Cards can not be played on bonuses with more than this much territories', 'int', 10, {
+		addSetting('MaxTerrs', 'Surveillance Cards can not be played on bonuses with more than this much territories', 'int', 10, {
 			minValue = 2,
 			maxValue = 50,
 			absoluteMax = 4000-- current wz max terrs allowed in map
