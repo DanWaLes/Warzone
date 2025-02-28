d4cdb380706fc5d528194dd89af10a24d5567e26
Dan <daniel.church@btinternet.com>
2025-02-28 15:19:09 +0000

    Update __mod.lua
    increase app version requirements for working with custom cards and custom fog levels

--- mods/Custom Card Package 2/__mod.lua
+++ mods/Custom Card Package 2/__mod.lua
@@ -1,12 +1,14 @@
 -- modified from https://github.com/DanWaLes/Warzone/tree/master/mods/libs/version
 
 -- https://www.warzone.com/wiki/Mod_API_Reference#Newer_API_features
--- https://www.warzone.com/wiki/Mod_API_Reference:GameOrderCustom OccursInPhaseOpt 5.22.0
+-- https://www.warzone.com/wiki/Mod_API_Reference:FogMod 5.33
+-- https://www.warzone.com/wiki/Mod_API_Reference:Custom_Cards 5.32.0.1
+-- https://www.warzone.com/wiki/Mod_Hooks#Client_Hooks Client_GameOrderCreated 5.32
 -- https://www.warzone.com/wiki/Mod_API_Reference:UI UI.InterceptNextTerritoryClick 5.17.0
 -- https://www.warzone.com/wiki/Mod_Hooks Server_AdvanceTurn_Order addNewOrder arg2 5.17.0
 
 MOD = {
 	name = 'Custom Card Package 2',
-	clientVersion = '5.22',
-	serverVersion = '5.22'
+	clientVersion = '5.33',
+	serverVersion = '5.33'
 };
